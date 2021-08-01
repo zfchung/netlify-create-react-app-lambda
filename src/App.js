532be6e9c0f306ccc6a9a59269cbf78bd8c5d614
ZF Chung (Zack) <zfchung1@gmail.com>
2021-08-01 17:54:45 +0800

    commented our LamdaDemo function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
+// class LambdaDemo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { loading: false, msg: null };
+//   }
 
-  handleClick = (api) => (e) => {
-    e.preventDefault();
+//   handleClick = (api) => (e) => {
+//     e.preventDefault();
 
-    this.setState({ loading: true });
-    fetch("/.netlify/functions/" + api)
-      .then((response) => response.json())
-      .then((json) => this.setState({ loading: false, msg: json.msg }));
-  };
+//     this.setState({ loading: true });
+//     fetch("/.netlify/functions/" + api)
+//       .then((response) => response.json())
+//       .then((json) => this.setState({ loading: false, msg: json.msg }));
+//   };
 
-  render() {
-    const { loading, msg } = this.state;
+//   render() {
+//     const { loading, msg } = this.state;
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>
-          {loading ? "Loading..." : "Call Lambda"}
-        </button>
-        <button onClick={this.handleClick("async-dadjoke")}>
-          {loading ? "Loading..." : "Call Async Lambda"}
-        </button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    );
-  }
-}
+//     return (
+//       <p>
+//         <button onClick={this.handleClick("hello")}>
+//           {loading ? "Loading..." : "Call Lambda"}
+//         </button>
+//         <button onClick={this.handleClick("async-dadjoke")}>
+//           {loading ? "Loading..." : "Call Async Lambda"}
+//         </button>
+//         <br />
+//         <span>{msg}</span>
+//       </p>
+//     );
+//   }
+// }
 
 class App extends Component {
   render() {
