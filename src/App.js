7fd21bbc3bdb13466601b708b477ea6b6e33dc5d
ZF Chung (Zack) <zfchung1@gmail.com>
2021-09-14 06:34:43 +0800

    adding img for 4 rules of simple design note

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import mindmap from "./mindmap-simplicity.png";
 
 // class LambdaDemo extends Component {
 //   constructor(props) {
@@ -44,6 +45,16 @@ class App extends Component {
         </header>
         <main className="App-main App-padding">
           <article>
+            <h1>Notes on 4 Rules of Simple Design</h1>
+            <p className="App-date">13th September 2021</p>
+            <p>
+              Taking notes from Corey Haines'{" "}
+              <i>Understanding 4 Rules of Simple Design</i>
+            </p>
+            <img src={mindmap} alt=""></img>
+          </article>
+          <hr />
+          <article>
             <h2>Notes on Refactoring</h2>
             <p className="App-date">29th August 2021</p>
             <p>
@@ -53,37 +64,36 @@ class App extends Component {
               </a>
             </p>
             <h3>What is Refactoring?</h3>
+            <ul>
+              <li>
+                Breaking code down to smaller components/factors, and then being
+                able to use those smaller components in an easier way.
+              </li>
+              <li>
+                Allow you to make changes with zero impact on user, the api, the
+                behaviour of your system. It will only change the internals.
+              </li>
+            </ul>
             <p>
-              <ul>
-                <li>
-                  Breaking code down to smaller components/factors, and then
-                  being able to use those smaller components in an easier way.
-                </li>
-                <li>
-                  Allow you to make changes with zero impact on user, the api,
-                  the behaviour of your system. It will only change the
-                  internals.
-                </li>
-              </ul>
               If Refactoring does not add new feature, new UI, what value does
               it bring to our customers?
             </p>
             <h3>Why do we want to refactor?</h3>
-            <p>
-              <ul>
-                <li>Software is most valuable when it's flexible</li>
-                <li>Flexibility leads to maintainability</li>
-                <li>
-                  The beauty of software, unlike other mediums, is that it is
-                  able to evolve as user needs changes.
-                </li>
-                <li>
-                  Software is no longer be able to evolve, when it becomes like
-                  a rusty screw with nut: "Tightly Coupled", "Rusted", "Corodded
-                  over time"
-                </li>
-              </ul>
-            </p>
+
+            <ul>
+              <li>Software is most valuable when it's flexible</li>
+              <li>Flexibility leads to maintainability</li>
+              <li>
+                The beauty of software, unlike other mediums, is that it is able
+                to evolve as user needs changes.
+              </li>
+              <li>
+                Software is no longer be able to evolve, when it becomes like a
+                rusty screw with nut: "Tightly Coupled", "Rusted", "Corodded
+                over time"
+              </li>
+            </ul>
+
             <h3>What about Rewrite?</h3>
             <p>
               Often time we are tempted by the siren song of rewrite when we see
@@ -114,94 +124,93 @@ class App extends Component {
             <h3>
               TDD: Red -{">"} Green -{">"} Refactor
             </h3>
-            <p>
-              <ul>
-                <li>
-                  When you only refactor when things went terribly wrong or
-                  until there is a palpable sense of inefficiency in our code
-                  base that you feel that you need to refactor to get our days,
-                  weeks back in our life.
-                </li>
-                <li>There are no good choices when you got to that point</li>
-                <li>TDD means that you are refactoring daily, hourly</li>
-                <li>
-                  We are constantly re-evaluating the design of our code and
-                  make changes along the way
-                </li>
-                <li>It gives software its value</li>
-                <li>
-                  Don't think about how do we rehaul the whole project. Think
-                  about how can I react on something little today/this
-                  morning/this afternoon.
-                </li>
-              </ul>
-            </p>
-            <h3>Goals fo Refactoring</h3>
-            <p>
-              <ul>
-                <li>
-                  We're on the job, we have a professional responsibility to set
-                  a goal of this refactoring exercise.
-                </li>
-                <li>
-                  The goal is we're looking for simplicity.
-                  <ul>
-                    <li>
-                      If the code that we are looking at is complex, it is
-                      disjointed, conjoined in ad places, it's no longer easy to
-                      explain to someone, not easy to reason about.
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  How to define simplicity: Kent Beck's 4 Rules of Simple Design
-                  <ul>
-                    <li>
-                      Test Passes
-                      <ul>
-                        <li>
-                          You cannot have simple code if you cannot verify the
-                          code works. Because it may lie to you if you do not
-                          have automated feedback on what it claims it does.
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      Expresses Intent
-                      <ul>
-                        <li>What we end up put in the software</li>
-                        <li>
-                          When we see a code that is rusty, we would be thinking
-                          "What is this person thinking". Exactly, we do not
-                          know what the person is thinking
-                        </li>
-                        <li>Easy for people to understand</li>
-                      </ul>
-                    </li>
-                    <li>
-                      No Duplication (DRY or SPOT)
-                      <ul>
-                        <li>Not about syntax or code, but about knowledge</li>
-                        <li>
-                          When requirement change and knowledge change, out of
-                          sync knowledge will break
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      Small
-                      <ul>
-                        <li>
-                          Remove elements that are not following the above 3
-                          rules
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </p>
+
+            <ul>
+              <li>
+                When you only refactor when things went terribly wrong or until
+                there is a palpable sense of inefficiency in our code base that
+                you feel that you need to refactor to get our days, weeks back
+                in our life.
+              </li>
+              <li>There are no good choices when you got to that point</li>
+              <li>TDD means that you are refactoring daily, hourly</li>
+              <li>
+                We are constantly re-evaluating the design of our code and make
+                changes along the way
+              </li>
+              <li>It gives software its value</li>
+              <li>
+                Don't think about how do we rehaul the whole project. Think
+                about how can I react on something little today/this
+                morning/this afternoon.
+              </li>
+            </ul>
+
+            <h3>Goals of Refactoring</h3>
+
+            <ul>
+              <li>
+                We're on the job, we have a professional responsibility to set a
+                goal of this refactoring exercise.
+              </li>
+              <li>
+                The goal is we're looking for simplicity.
+                <ul>
+                  <li>
+                    If the code that we are looking at is complex, it is
+                    disjointed, conjoined in ad places, it's no longer easy to
+                    explain to someone, not easy to reason about.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                How to define simplicity: Kent Beck's 4 Rules of Simple Design
+                <ul>
+                  <li>
+                    Test Passes
+                    <ul>
+                      <li>
+                        You cannot have simple code if you cannot verify the
+                        code works. Because it may lie to you if you do not have
+                        automated feedback on what it claims it does.
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    Expresses Intent
+                    <ul>
+                      <li>What we end up put in the software</li>
+                      <li>
+                        When we see a code that is rusty, we would be thinking
+                        "What is this person thinking". Exactly, we do not know
+                        what the person is thinking
+                      </li>
+                      <li>Easy for people to understand</li>
+                    </ul>
+                  </li>
+                  <li>
+                    No Duplication (DRY or SPOT)
+                    <ul>
+                      <li>Not about syntax or code, but about knowledge</li>
+                      <li>
+                        When requirement change and knowledge change, out of
+                        sync knowledge will break
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    Small
+                    <ul>
+                      <li>
+                        Remove elements that are not following the above 3 rules
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </article>
+          <hr />
           <article>
             <h2>Set up plain static site using Netlify Create React App</h2>
             <p className="App-date">1st August 2021</p>
